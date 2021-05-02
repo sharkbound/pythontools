b2450c28d71b023b753521b95ec03fe7fc178d26
sharkbound <laptopblaster@gmail.com>
2021-05-02 00:45:56 -0500

    added code to hide cursor in quiz example

--- examples/prompttoolkit/interative_quiz/run_prompttoolkit_quiz.py
+++ examples/prompttoolkit/interative_quiz/run_prompttoolkit_quiz.py
@@ -1,4 +1,5 @@
 from prompt_toolkit import PromptSession, Application
+from prompt_toolkit.filters import Always
 from prompt_toolkit.keys import Keys
 from prompt_toolkit.key_binding import KeyBindings, KeyPressEvent
 from prompt_toolkit.styles import Style
@@ -31,11 +32,7 @@ class OptionsTextProvider:
     def tokens(self):
         if self.answered:
             return [('class:quiz_item_selected', f'YOU CHOOSE: "{self.current_item}"')]
-
-        return [
-            self._token_for_index(i, v)
-            for i, v in enumerate(self.choices)
-        ]
+        return [self._token_for_index(i, v) for i, v in enumerate(self.choices)]
 
 
 class Quiz:
@@ -47,7 +44,8 @@ class Quiz:
         self.keybindings = KeyBindings()
         self.style = Style([
             ('quiz_item_selected', f'bg:{self.BG} fg:{self.FG}'),
-            ('quiz_item_not_selected', f'bg:{self.FG} fg:{self.BG}')
+            ('quiz_item_not_selected', f'bg:{self.FG} fg:{self.BG}'),
+            ('empty', '')
         ])
 
         self.keybindings.add(Keys.Up, eager=True)(lambda _: self.text_provider.up())
@@ -62,5 +60,30 @@ class Quiz:
 
 quiz = Quiz(OptionsTextProvider('a', 'b', 'c'))
 session = PromptSession(message=quiz.text_provider.tokens)
+
+# hides the cursor
+session.layout.container.get_children()[0].content.get_children()[1].content.always_hide_cursor = Always()
+
 app = Application(layout=session.layout, key_bindings=quiz.keybindings, style=quiz.style)
 print(f'RESULT: {app.run()}')
+
+# from questionary on pypi
+# def _fix_unecessary_blank_lines(ps: PromptSession) -> None:
+#     """This is a fix for additional empty lines added by prompt toolkit.
+#
+#     This assumes the layout of the default session doesn't change, if it
+#     does, this needs an update."""
+#
+#     default_container = ps.layout.container
+#
+#     default_buffer_window = (
+#         default_container
+#     )
+#
+#     from prompt_toolkit.layout import Window
+#     assert isinstance(default_buffer_window, Window)
+#     # this forces the main window to stay as small as possible, avoiding
+#     # empty lines in selections
+#     from prompt_toolkit.filters import Always
+#     default_buffer_window.dont_extend_height = Always()
+#     default_buffer_window.always_hide_cursor = Always()
