5594fdf77031a9717a6e14b8938b53d1a2a2d610
sharkbound <laptopblaster@gmail.com>
2022-07-12 10:24:18 -0500

    histograms for go.Scatter()

--- examples/plotly_example/plotly_scatter_markers.py
+++ examples/plotly_example/plotly_scatter_markers.py
@@ -3,15 +3,14 @@ import numpy as np
 
 MAX = 2
 MIN = 0
-npstuff = np.linspace(MIN, MAX, 201)
+npstuff = np.linspace(MIN, MAX, 70)
 
 nums = [2 ** (1 + n) for n in npstuff]
 
-nums2 = list(set([2 ** (1 + int(n)) for n in npstuff]))
-nums2.sort()
+nums2 = sorted(set([2 ** (1 + int(n)) for n in npstuff]))
 
 fig = go.Figure()
-fig.add_trace(go.Scatter(x=npstuff, y=nums, mode='lines+markers'))
+fig.add_trace(scatter_trace := go.Scatter(x=npstuff, y=nums, mode='lines+markers'))
 fig.add_trace(go.Line(x=np.linspace(MIN, MAX, len(nums2)), y=nums2))
+fig.add_trace(go.Histogram(x=npstuff))
 fig.show()
-
